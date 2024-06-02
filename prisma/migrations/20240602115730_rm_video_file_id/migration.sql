96ec8d7e68797e248097beaf3843f40a4abd2f68
Oleh Voloshyn <bazisc.qa@gmail.com>
2024-06-02 16:13:13 +0300

    update

---
+++ prisma/migrations/20240602115730_rm_video_file_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `video_file_id` on the `processed_videos` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "processed_videos" DROP COLUMN "video_file_id";
